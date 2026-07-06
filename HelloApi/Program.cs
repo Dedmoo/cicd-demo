2096b01e145db95e42b01f6864914ec554f3d38a
Mehmet Serin <Dedmoo@users.noreply.github.com>
2026-07-06 11:41:30 +0300

    test mesaji

--- HelloApi/Program.cs
+++ HelloApi/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddSingleton<GreetingService>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Merhab33 Creamobile! CI/CD pipeline WSL uzerinde calisiyor.");
+app.MapGet("/", () => "Merhab34 Creamobile! CI/CD pipeline WSL uzerinde calisiyor.");
 
 app.MapGet("/greet/{name}", (string name, GreetingService svc) => svc.Greet(name));
 
