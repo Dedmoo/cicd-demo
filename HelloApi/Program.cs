c4957001b356b1d545b39fedaa73e72ce5ba45a8
Mehmet Serin <Dedmoo@users.noreply.github.com>
2026-07-06 15:59:06 +0300

    DENEME1234

--- HelloApi/Program.cs
+++ HelloApi/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddSingleton<GreetingService>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Merhaba Creamobile! deneme 33 - otomatik deploy calisiyor.");
+app.MapGet("/", () => "Merhaba Creamobile! deneme 1234 - otomatik deploy calisiyor.");
 
 app.MapGet("/greet/{name}", (string name, GreetingService svc) => svc.Greet(name));
 
